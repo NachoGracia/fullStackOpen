4d5581ed229524d6ec716c1f775500e1694fccce
NachoGracia <gracianacho@gmail.com>
2024-08-07 18:44:35 +0200

    5.1-5.4 exercises completed

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,3 +1,4 @@
+import "./app.css";
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
@@ -8,31 +9,61 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
-
-  const [userToCompare, setUserToCompare] = useState("");
   const [userLogged, setUserLogged] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  console.log("🚀 ~ App ~ blogs:", blogs);
+  const [newBlog, setNewBlog] = useState(false);
+
+  // console.log("🚀 ~ App ~ user:", user);
+
   const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
   const [content, setContent] = useState("");
   const [likes, setLikes] = useState(0);
   const [important, setImportant] = useState(false);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    const getPrevUserLogged = async () => {
+      const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
 
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      console.log("🚀 ~ useEffect ~ user:", user);
+      if (loggedUserJSON) {
+        const userInLocalStorage = JSON.parse(loggedUserJSON);
+        // console.log("🚀 userInLocalStorage:", userInLocalStorage);
 
-      setUser(user);
-      handleLoginOk();
-    }
+        try {
+          const userList = await userService.user();
+          // console.log("🚀 userList:", userList);
+
+          const userLogged = userList.find(
+            (user) => user.username === userInLocalStorage.username
+          );
+          // console.log("🚀 ~ getPrevUserLogged ~ userLogged:", userLogged);
+
+          if (userLogged) {
+            setUser(userLogged);
+            blogService.setToken(userInLocalStorage.token);
+          }
+        } catch (error) {
+          console.error("Error fetching users:", error);
+        }
+      }
+    };
+    getPrevUserLogged();
   }, []);
 
   useEffect(() => {
-    if (userLogged) {
-      handleLoginOk();
+    fetchBlogs();
+    setNewBlog(false);
+  }, [newBlog]);
+
+  const fetchBlogs = async () => {
+    try {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
     }
-  }, [userLogged, user]);
+  };
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -42,17 +73,32 @@ const App = () => {
         username,
         password,
       });
+      try {
+        const userList = await userService.user();
 
-      window.localStorage.setItem(
-        "loggedBlogAppUser",
-        JSON.stringify(userToLog)
-      );
-      blogService.setToken(userToLog.token);
-      setUser(userToLog);
-      setUsername("");
-      setPassword("");
-      setUserLogged(true);
-      setUserToCompare(userToLog.username);
+        const userLogged = userList.find(
+          (user) => user.username === userToLog.username
+        );
+        // console.log("🚀 ~ handleLogin ~ userLogged:", userLogged);
+        setUser(userLogged);
+        setUsername("");
+        setPassword("");
+        blogService.setToken(userToLog.token);
+        window.localStorage.setItem(
+          "loggedBlogAppUser",
+          JSON.stringify(userToLog)
+        );
+
+        try {
+        } catch (error) {
+          setErrorMessage("login failed");
+        }
+      } catch (exception) {
+        setErrorMessage("user not found");
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      }
     } catch (exception) {
       setErrorMessage("Wrong credentials");
       setTimeout(() => {
@@ -61,39 +107,45 @@ const App = () => {
     }
   };
 
-  const handleLoginOk = async (event) => {
-    try {
-      const userList = await userService.user();
-      const userLogged = userList.find(
-        (user) => user.username === userToCompare
-      );
-
-      setUser(userLogged);
-    } catch (exception) {
-      setErrorMessage("login failed");
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
-    }
-  };
-
-  const handleNewBlog = (event) => {
+  const handleNewBlog = async (event) => {
     event.preventDefault();
     const newBlogObject = {
       content: content,
       likes: likes,
       important: important,
     };
-    blogService.create(newBlogObject);
-    setContent("");
-    setLikes(0);
-    setImportant(false);
-  };
 
+    try {
+      const prevBlogs = await blogService.getAll();
+      console.log("🚀 ~ handleNewBlog ~ prevBlogs:", prevBlogs);
+
+      await blogService.create(newBlogObject);
+      setNewBlog(true);
+      setContent("");
+      setLikes(0);
+      setImportant(false);
+
+      const updatedBlogs = await blogService.getAll();
+      setBlogs(updatedBlogs);
+
+      if (updatedBlogs.length > prevBlogs.length) {
+        setSuccessMessage("New blog added! ✔");
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 5000);
+      }
+    } catch (error) {
+      console.error("Error creating new blog:", error);
+
+      setErrorMessage("Error creating new blog ❌");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
   const handleLogOut = () => {
     window.localStorage.removeItem("loggedBlogAppUser");
     setUser(null);
-    setUserLogged(false);
   };
 
   const loginForm = () => (
@@ -151,19 +203,20 @@ const App = () => {
     <>
       <h2>blogs</h2>
 
-      {errorMessage && <div>{errorMessage}</div>}
-      {!userLogged ? (
+      {!user ? (
         loginForm()
       ) : (
-        <div>
+        <div className="userContainer">
           <p>{user.username} logged-in</p>
-          {user?.blogs?.map((blog) => (
+          {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
           {<button onClick={handleLogOut}>logout</button>}
         </div>
       )}
-      {!userLogged ? null : blogForm()}
+      {successMessage && <div className="successMessage">{successMessage}</div>}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+      {user && blogForm()}
     </>
   );
 };
