df18686aecd7f3f2ca972b0e07b2d8d1c2de96ca
NachoGracia <gracianacho@gmail.com>
2024-08-08 18:46:51 +0200

    blogForm Component ok refactorized

--- part5/src/components/BlogForm.jsx
+++ part5/src/components/BlogForm.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import blogService from "../services/blogs";
 
 const BlogForm = ({
-  handleNewBlog,
-  content,
-  likes,
-  important,
-  handleContent,
-  handleLikes,
-  handleImportant,
+  setNewBlog,
+  setBlogs,
+  setErrorMessage,
+  setSuccessMessage,
 }) => {
+  const [content, setContent] = useState("");
+  const [likes, setLikes] = useState(0);
+  const [important, setImportant] = useState(false);
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault();
+    const newBlogObject = {
+      content: content,
+      likes: likes,
+      important: important,
+    };
+
+    try {
+      const prevBlogs = await blogService.getAll();
+      console.log("🚀 ~ handleNewBlog ~ prevBlogs:", prevBlogs);
+
+      await blogService.create(newBlogObject);
+      setNewBlog(true);
+      setContent("");
+      setLikes(0);
+      setImportant(false);
+
+      const updatedBlogs = await blogService.getAll();
+      setBlogs(updatedBlogs);
+
+      if (updatedBlogs.length > prevBlogs.length) {
+        setSuccessMessage("New blog added! ✔");
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 5000);
+      }
+    } catch (error) {
+      console.error("Error creating new blog:", error);
+
+      setErrorMessage("Error creating new blog ❌");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
   return (
     <div>
       <h2>create new Blog</h2>
@@ -17,18 +56,18 @@ const BlogForm = ({
           name="content"
           type="text"
           value={content}
-          onChange={handleContent}
+          onChange={(e) => setContent(e.target.value)}
         />
         <input
           type="number"
           name="likes"
           value={likes}
-          onChange={handleLikes}
+          onChange={(e) => setLikes(Number(e.target.value))}
         />
         <input
           type="checkbox"
           checked={important}
-          onChange={handleImportant}
+          onChange={(e) => setImportant(e.target.checked)}
           name="important"
         />
         <button type="submit">save</button>
